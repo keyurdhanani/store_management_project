0262f2676d02650f2a6be341eb312f5c542e8dbb
Keyur Dhanani <154542760+keyurdhanani@users.noreply.github.com>
2025-11-24 08:07:28 +0530

    u

--- store_management_project/store_management_project/settings.py
+++ store_management_project/store_management_project/settings.py
@@ -84,8 +84,16 @@ WSGI_APPLICATION = 'store_management_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'store_management_db',       
+        'USER': 'root',
+        'PASSWORD': '1226',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        }
     }
 }
 
